42072d6d1de3ab8cef991b3dc9566794b168f854
Iulii1102 <juliaborisenko1102@mail.ru>
2022-02-21 15:11:57 +0100

    grobes Gerüst der Seitenreihenfolge

--- ressources/js/book/BookLoader.js
+++ ressources/js/book/BookLoader.js
@@ -3,20 +3,39 @@ import Page from "./Page.js";
 const BOOK_PAGE_URLS = [
     "./pages/empty.html",
     "./pages/cover.html",
+    "./pages/empty.html",
     "./pages/preface.html",
     "./pages/demographics.html",
     "./pages/self-assessment-java.html",
-    "./pages/empty.html",/*
-    "./pages/toc.html",*/
+    "./pages/empty.html", 
+    "./pages/toc.html",
+    "./pages/page-05/chapter.html",
+    "./pages/page-05/binary_tree_1.html",
+    "./pages/page-05/binary_tree_2.html",
 
-        "./pages/page-05/index.html",
-        "./pages/page-06/index.html",
-        "./pages/page-07/index.html",
-        "./pages/page-08/index.html",
-        "./pages/page-09/index.html",
-        "./pages/page-10/index.html",
-        "./pages/page-11/index.html",
-        "./pages/page-12/index.html",
+    "./pages/page-06/binary_tree_3.html",
+    "./pages/page-06/binary_tree_4.html",
+
+    "./pages/page-07/binary_tree_5.html",
+
+    "./pages/page-08/chapter.html",
+    "./pages/page-08/test_1.html",
+    "./pages/page-08/test_2.html",
+
+    "./pages/page-09/test_3_4.html",
+    "./pages/page-09/test_5.html",
+
+    "./pages/page-10/test_6.html",
+    "./pages/page-10/test_7.html",
+    "./pages/empty.html",
+
+    "./pages/page-11/experience_1.html",
+    "./pages/page-11/experience_2.html",
+    // "./pages/page-08/index.html",
+    // "./pages/page-09/index.html",
+    // "./pages/page-10/index.html",
+    // "./pages/page-11/index.html",
+    "./pages/page-12/index.html",
   ],
   ROOT_ELEMENT = document.querySelector(".book");
 
